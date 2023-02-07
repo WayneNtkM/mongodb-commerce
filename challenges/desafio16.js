e3be31b39e7406009f34f9ee1b8b0c61005fa13c
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-02-07 18:12:52 -0300

    feat: requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany({
+  nome: { $regex: 'Big Mac' }
+}, {
+  $set: { ultimaModificacao: Date() }
+});
+
+
+db.produtos.find({
+  ultimaModificacao: { $exists: true }
+}, {
+  _id: 0, nome: 1
+});
